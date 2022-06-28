9c332679e078af969acdaef46fb49938a23aa84f
Furkan <furkanahmetkara.fk@gmail.com>
2022-06-28 10:25:21 +0300

    Update main.py
    main.py beautified

--- FireDetection/main.py
+++ FireDetection/main.py
@@ -1,16 +1,233 @@
-# This is a sample Python script.
+################################################################################
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2
+import os
+import sys
+import math
+import time
+from datetime import datetime
+
+################################################################################
+import numpy as np
+import tensorflow
+
+import tflearn
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from tflearn.layers.core import *
+from tflearn.layers.conv import *
+from tflearn.layers.normalization import *
+from tflearn.layers.estimator import regression
+
+from sklearn.model_selection import train_test_split
+
+################################################################################
+from tqdm import tqdm
+
+CATEGORIES = ["fire", "nofire"]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def construct_firenet(x, y, training=False):
+    # Build network as per architecture in [Dunnings/Breckon, 2018]
 
+    network = tflearn.input_data(shape=[None, y, x, 3], dtype=tensorflow.float32)
+
+    network = conv_2d(network, 64, 5, strides=4, activation='relu')
+
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = conv_2d(network, 128, 4, activation='relu')
+
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = conv_2d(network, 256, 1, activation='relu')
+
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = fully_connected(network, 4096, activation='tanh')
+    if (training):
+        network = dropout(network, 0.5)
+
+    network = fully_connected(network, 4096, activation='tanh')
+    if (training):
+        network = dropout(network, 0.5)
+
+    network = fully_connected(network, 2, activation='softmax')
+
+    # if training then add training hyperparameters
+
+    if (training):
+        network = regression(network, optimizer='momentum',
+                             loss='categorical_crossentropy',
+                             learning_rate=0.001)
+
+    # constuct final model
+
+    model = tflearn.DNN(network, checkpoint_path='firenet',
+                        max_checkpoints=1, tensorboard_verbose=2)
+
+    return model
+
+
+def read_data(directory):
+    data_set = []
+    for category in CATEGORIES:
+        path = os.path.join(directory, category)  # create path
+        class_num = CATEGORIES.index(category)  # get the classification
+
+        for img in tqdm(os.listdir(path)):  # iterate over each image
+            try:
+                img_array = cv2.imread(os.path.join(path, img))  # convert to array
+                new_array = cv2.resize(img_array, (224, 224), cv2.INTER_AREA)  # resize to normalize data size
+                data_set.append([new_array, CATEGORIES[class_num]])  # add this to our training_data
+            except Exception as e:
+                print(e)
+    return data_set
+
+
+def run():
+    
+    ################################################################################
+
+    # construct and display model
+
+    model = construct_firenet(224, 224, training=False)
+    print("Constructed FireNet ...")
+
+    model.load(os.path.join("models/FireNet", "firenet"),weights_only=True)
+    print("Loaded CNN network weights ...")
+
+    ################################################################################
+    # train data set
+    #history = model.fit(x_train, y_train, 5,validation_set=(x_validate, y_validate))
+
+    ################################################################################
+
+    # Manually save model
+    #model.save("last_model.tfl")
+    """
+    print("[INFO] evaluating network...")
+    predictions = model.predict(x_test)
+    print(classification_report(y_test.argmax(axis=1),
+                                predictions.argmax(axis=1), target_names=CATEGORIES))"""
+
+    ################################################################################
+
+    # network input sizes
+
+    rows = 224
+    cols = 224
+
+    # display and loop settings
+
+    windowName = "Live Fire Detection - FireNet CNN";
+    keepProcessing = True;
+
+    ################################################################################
+
+
+    # load video file from first command line argument
+
+    video = cv2.VideoCapture("shortvideoplayback.mp4")
+    print("Loaded video ...")
+
+    # create window
+
+    cv2.namedWindow(windowName, cv2.WINDOW_NORMAL);
+
+    # get video properties
+
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH));
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = video.get(cv2.CAP_PROP_FPS)
+    frame_time = round(1000 / fps);
+    logfile = open("firelog.txt",'w')
+
+    gcs_conn = mavutil.mavlink_connection('tcp:localhost:15795', input=False)
+    gcs_conn.wait_heartbeat()
+    print("Heartbeat from system (system %u component %u)" % (gcs_conn.target_system, gcs_conn.target_system))
+
+
+    pass_count = 0;
+
+    while (keepProcessing):
+
+        # start a timer (to see how long processing and display takes)
+
+        start_t = cv2.getTickCount();
+        
+
+        # get video frame from file, handle end of file
+        start_time = time.monotonic()
+        ret, frame = video.read()
+        if not ret:
+            print("... end of video file reached");
+            break;
+
+        # re-size image to network input size and perform prediction
+
+        small_frame = cv2.resize(frame, (rows, cols), cv2.INTER_AREA)
+
+        # perform prediction on the image frame which is:
+        # - an image (tensor) of dimension 224 x 224 x 3
+        # - a 3 channel colour image with channel ordering BGR (not RGB)
+        # - un-normalised (i.e. pixel range going into network is 0->255)
+
+        output = model.predict([small_frame])
+
+        # label image based on prediction
+
+        if round(output[0][0]) == 1:
+            if pass_count <= 10:
+                pass_count += 1
+        else:
+            if pass_count >= 0:
+                pass_count -= 1
+               
+        if pass_count > 5 :
+
+            gcs_msg = b"fire detected."
+            gcs_conn.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_CRITICAL, gcs_msg)
+            print(gcs_msg)
+            now = datetime.now()
+            logfile.write(now.strftime("%H:%M:%S"))
+            cv2.rectangle(frame, (0, 0), (width, height), (0, 0, 255), 50)
+            cv2.putText(frame, 'FIRE', (int(width / 16), int(height / 4)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255), 10, cv2.LINE_AA);
+        else:
+            gcs_msg = b"clear."
+            gcs_conn.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_CRITICAL, gcs_msg)
+            print(gcs_msg)
+            cv2.rectangle(frame, (0, 0), (width, height), (0, 255, 0), 50)
+            cv2.putText(frame, 'CLEAR', (int(width / 16), int(height / 4)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255), 10, cv2.LINE_AA);
+        
+
+        # stop the timer and convert to ms. (to see how long processing and display takes)
+
+        stop_t = ((cv2.getTickCount() - start_t) / cv2.getTickFrequency()) * 1000;
+
+        # image display and key handling
+
+        cv2.imshow(windowName, frame);
+        elapsed_sec = (time.monotonic() - start_time)
+        fps = 1/elapsed_sec
+        print("\nFPS:")
+        print(fps)
+        
+        # wait fps time or less depending on processing time taken (e.g. 1000ms / 25 fps = 40 ms)
+        key = cv2.waitKey(max(2, frame_time - int(math.ceil(stop_t)))) & 0xFF;
+        if (key == ord('x')):
+            keepProcessing = False;
+        elif (key == ord('f')):
+            cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
+    logfile.close()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
